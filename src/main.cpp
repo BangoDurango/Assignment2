ec3c9ed6e59cc18d99e316f30de107c0417a4348
BangoDurango <keving@email.arizona.edu>
2016-09-29 14:29:09 -0700

    Setup
    Setting up local branch

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main (int argc, char* argv[]){
+	//std::cout << "Test" << endl;	
+	
+	if (argc != 3) {
+		std::cout << "Incorrect number of arguments." << endl;
+		return 0;
+	}
+	cout << argv[0] << endl;
+
+	ifstream inFile(argv[1]);
+	
+	string line;
+
+	if (inFile.is_open() && inFile.good()) cout << "File Opened!" << endl;
+
+	while (getline(inFile, line)) {
+		cout << line << endl;
+	}
+	inFile.close();
+	//string netlistFile = argv[1];
+	//string verilogFile = argv[2];
+	return 0;
+}
